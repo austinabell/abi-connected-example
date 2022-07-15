138f9c258f6b0a354a7995d024f4637f8bca341a
austinabell <austinabell8@gmail.com>
2022-07-14 21:12:35 -0400

    very basic generic functionality with string inputs

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,63 +1,16 @@
 import "regenerator-runtime/runtime";
-import React, { useState, useEffect, FormEvent } from "react";
-import Big from "big.js";
+import React from "react";
 import Form from "./components/Form";
 import { Account, WalletConnection } from "near-api-js";
-import { Contract, ABIFunction } from "near-abi-client-js";
-
-
-// interface FormElements extends HTMLFormControlsCollection {
-//   fieldset: HTMLInputElement;
-//   message: HTMLInputElement;
-// }
+import { ABIFunction, AnyContract } from "near-abi-client-js";
 
 interface AppState {
-  contract: UndefinedContract;
+  contract: AnyContract;
   wallet: WalletConnection;
   account?: Account;
 }
 
-export interface UndefinedContract extends Contract {
-  // Allow any other types on the contract that are not defined.
-  // This is ideally not needed when TS generated from ABI.
-  [x: string]: any;
-}
-
 export default function App({ contract, wallet, account }: AppState) {
-  // const [status, setStatus] = useState(null);
-
-  // useEffect(() => {
-  //   async function fetchStatus() {
-  //     if (account?.accountId) {
-  //       const status = await contract.get_status(account.accountId).view();
-
-  //       setStatus(status);
-  //     }
-  //   }
-
-  //   fetchStatus();
-  // }, []);
-
-  // const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
-
-  //   const target = event.target as HTMLFormElement;
-  //   const { fieldset, message } = target.elements as FormElements;
-  //   fieldset.disabled = true;
-
-  //   await contract
-  //     .set_status(message.value)
-  //     .callFrom(account!, { gas: BOATLOAD_OF_GAS });
-
-  //   const status = await contract.get_status(account!.accountId).view();
-
-  //   setStatus(status);
-
-  //   message.value = "";
-  //   fieldset.disabled = false;
-  //   message.focus();
-  // };
-
   const signIn = () => {
     wallet.requestSignIn(
       { contractId: contract.contractId, methodNames: ["set_status"] },
@@ -71,16 +24,8 @@ export default function App({ contract, wallet, account }: AppState) {
   };
 
   function abiFnToField(fn: ABIFunction) {
-    const onSubmit = (e: React.SyntheticEvent) => {
-      e.preventDefault();
-
-      const target = e.target as typeof e.target & {
-        email: { value: string };
-        password: { value: string };
-      };
-    };
     return (
-      <Form contract={contract} account={account} func={fn} />
+      <Form contract={contract} account={account} func={fn} key={fn.name} />
     );
   }
 
@@ -107,7 +52,6 @@ export default function App({ contract, wallet, account }: AppState) {
       </header>
 
       {account?.accountId && <div>{forms}</div>}
-      {/* {account?.accountId && <Form onSubmit={onSubmit} />} */}
     </main>
   );
 }
--- frontend/components/Form.tsx
+++ frontend/components/Form.tsx
@@ -1,53 +1,69 @@
-import Big from "big.js";
-import { ABIFunction } from "near-abi-client-js";
-import { Account, WalletConnection } from "near-api-js";
-import React, { FormEvent, FormEventHandler, useState } from "react";
-import { UndefinedContract } from "../App";
+import { ABIFunction, ABIParameterInfo, AnyContract } from "near-abi-client-js";
+import { Account } from "near-api-js";
+import React, { FormEvent, useState } from "react";
 
 // Large amount to make sure things pass
-const TX_GAS = Big(30_000_000_000_000);
+const TX_GAS = 30_000_000_000_000;
 
 interface FormParams {
-  contract: UndefinedContract;
+  contract: AnyContract;
   account?: Account;
   func: ABIFunction;
 }
 
 export default function Form({ contract, account, func }: FormParams) {
-  const [form, setForm] = useState({});
   const [out, setOut] = useState<any>(undefined);
 
   const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     const target = event.target as HTMLFormElement;
-    const { fieldset, message } = target.elements as any;
-    fieldset.disabled = true;
+    const elements = target.elements as any;
+    elements.fieldset.disabled = true;
 
-    await contract
-      .set_status(message.value)
-      .callFrom(account!, { gas: TX_GAS });
+    // Collect all input from fields based on the order of params in ABI
+    //? To be safe on this, we could assign it to an object. Order should be sufficient though
+    let params: any[] = func.params
+      ? func.params.map((p) => elements[p.name].value)
+      : [];
 
-    const status = await contract.get_status(account!.accountId).view();
+    const method = contract[func.name](...params);
+    let res;
+    try {
+      if (!func.is_view) {
+        res = await method.callFrom(account!, {
+          gas: TX_GAS,
+        });
+      } else {
+        res = await method.view();
+      }
+      setOut(res);
+    } catch (e: any) {
+      setOut(`ERROR: ${e}`);
+    }
 
-    setOut(status);
+    elements.fieldset.disabled = false;
+  };
 
-    message.value = "";
-    fieldset.disabled = false;
-    message.focus();
+  func.params;
+
+  const paramToField = (param: ABIParameterInfo) => {
+    return (
+      <p className="highlight" key={param.name}>
+        <label htmlFor="message">{param.name}</label>
+        {/* TODO change this from just string input */}
+        <input autoComplete="off" autoFocus id={param.name} />
+      </p>
+    );
   };
 
   return (
     <form onSubmit={onSubmit}>
       <fieldset id="fieldset">
         <p>{func.name}</p>
-        <p className="highlight">
-          <label htmlFor="message">TODO</label>
-          <input autoComplete="off" autoFocus id="message" required />
-        </p>
-        <button type="submit">Call</button>
-        {/* TODO View button */}
-        <p>{}</p>
+        {func.params ? func.params.map((p) => paramToField(p)) : undefined}
+        <button type="submit">{func.is_view ? "View" : "Call"}</button>
+        <p>{out}</p>
       </fieldset>
     </form>
   );
